df90bbfa9fc8c96fe0546cfe8568e81b1d9d3d08
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2020-06-26 23:11:27 -0500

    v1

---
+++ WebAPIService/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIService.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public String ProductName { get; set; }
+
+        public Nullable<int> Quantity { get; set; }
+
+        public Nullable<int> Price { get; set; }
+    }
+}
---
+++ WebAPIService2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="WebAPIService2.WebApiApplication" Language="VB" %>
